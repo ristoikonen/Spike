9f07c204c1bf1e539efc21e288add9111628bc7f
Risto <risto@gmx.com>
2019-11-27 15:50:10 +1100

    Add files via upload

---
+++ SpikeDetectionConsoleApp/DataStructures/ProductSalesData.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.ML.Data;
+
+namespace SpikeDetection.DataStructures
+{
+    public class ProductSalesData
+    {
+        [LoadColumn(0)]
+        public string Month;
+
+        [LoadColumn(1)]
+        public float numSales;
+    }
+
+    public class PopData
+    {
+        [LoadColumn(2)]
+        public string Time;
+
+        [LoadColumn(3)]
+        public float Value;
+    }
+
+}
---
+++ SpikeDetectionConsoleApp/DataStructures/ProductSalesPrediction.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.ML.Data;
+
+namespace SpikeDetection.DataStructures
+{
+    public class ProductSalesPrediction
+    {
+        //vector to hold alert,score,p-value values
+        [VectorType(3)]
+        public double[] Prediction { get; set; }
+    }
+
+    public class PopPrediction
+    {
+        //vector to hold alert,score,p-value values
+        [VectorType(6)]
+        public double[] Prediction { get; set; }
+    }
+}
